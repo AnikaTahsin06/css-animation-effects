840a649bcf7879b2051664dda9538b43bbc32275
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-07-05 22:55:00 +0600

    animated scroll and animated button created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+import '../styles/components/animated-button.css';
 import '../styles/components/wavy-circle.css';
 import '../styles/utils.css';
