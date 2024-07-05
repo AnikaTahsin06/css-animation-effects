ceeb10873e817d3fb4209532b9d717a37ad79e6d
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-07-05 23:03:27 +0600

    fixed page not found issue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/css-animation-effects/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        animatedScroll: resolve(__dirname, 'pages/animated-scroll.html')
+        // Add more pages as needed
+      }
+    }
+  }
 });
