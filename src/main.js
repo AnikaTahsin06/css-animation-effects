5bf0a8e4f97e402d09cb9a9aa9537a45d2448cbc
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-07-06 23:41:53 +0600

    neumorphism created

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/animated-button.css';
 import '../styles/components/wavy-circle.css';
+import '../styles/components/neumorphism.css';
 import '../styles/utils.css';
